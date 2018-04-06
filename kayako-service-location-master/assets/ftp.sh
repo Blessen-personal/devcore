88ca2a3f68f9dceb4931c8ae6b43c6c5f01235dc
kaushik H S <kaushik@kaushiks-MacBook-Pro.local>
2018-04-06 22:01:10 +0530

    adding location as exec into ftp

--- kayako-service-location-master/assets/ftp.sh
+++ kayako-service-location-master/assets/ftp.sh
@@ -5,9 +5,8 @@ bin
 get city.mmdb /geo-db/city.mmdb
 get isp.mmdb  /geo-db/isp.mmdb
 get conn.mmdb /geo-db/conn.mmdb
-get vendor.tar.gz /golang/src/github.com/kayako/service-location/vendor.tar.gz
+get location.tar.gz /tmp/location.tar.gz
 bye
 EOF
 
-cd /golang/src/github.com/kayako/service-location/ && tar -xvf vendor.tar.gz  &&  make geo && mv ./build/location /bin/location
-rm -rf /golang
+cd /tmp && tar -xvf location.tar.gz && mv ./location /bin/location
