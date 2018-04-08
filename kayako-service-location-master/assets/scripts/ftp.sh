b75e1b9b11483567677d763a2afdc6f07b008b1c
kaushik H S <kaushik@kaushiks-MacBook-Pro.local>
2018-04-08 10:10:15 +0530

    removing all the scripts and folders after installation

--- kayako-service-location-master/assets/scripts/ftp.sh
+++ kayako-service-location-master/assets/scripts/ftp.sh
@@ -7,9 +7,8 @@ get city.mmdb /geo-db/city.mmdb
 get isp.mmdb  /geo-db/isp.mmdb
 get conn.mmdb /geo-db/conn.mmdb
 get location.tar.gz /tmp/location.tar.gz
-
 bye
 EOF
-
-
+# Extracting the executable & removing the compressed file
 cd /tmp && tar -C /usr/local -xzf go1.7.linux-amd64.tar.gz && tar -xvf location.tar.gz && mv ./location /bin/location
+rm -rf /tmp/go1.7.linux-amd64.tar.gz /tmp/location.tar.gz
