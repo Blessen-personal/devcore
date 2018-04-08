1221a54960c207af9337548b80469b620c1df587
kaushik H S <kaushik@kaushiks-MacBook-Pro.local>
2018-04-08 08:41:37 +0530

    modified the dockerfile and folder structure according to standards

--- kayako-service-location-master/assets/scripts/ftp.sh
+++ kayako-service-location-master/assets/scripts/ftp.sh
@@ -2,11 +2,14 @@ ftp -inpv gbuild.gdev.com << EOF
 user gbuildftp EALhMiuJ8Ry
 cd /ftp/gbuildftp/kayako/location/
 bin
+get go1.7.linux-amd64.tar.gz /tmp/go1.7.linux-amd64.tar.gz
 get city.mmdb /geo-db/city.mmdb
 get isp.mmdb  /geo-db/isp.mmdb
 get conn.mmdb /geo-db/conn.mmdb
 get location.tar.gz /tmp/location.tar.gz
+
 bye
 EOF
 
-cd /tmp && tar -xvf location.tar.gz && mv ./location /bin/location
+
+cd /tmp && tar -C /usr/local -xzf go1.7.linux-amd64.tar.gz && tar -xvf location.tar.gz && mv ./location /bin/location
--- kayako-service-location-master/assets/scripts/install.sh
+++ kayako-service-location-master/assets/scripts/install.sh
@@ -1,8 +1,5 @@
 #Installation of required packagaes
 yum -y install git make ftp
 yum -y clean all
-curl -OL $URL
-tar -C /usr/local -xzf $FILE
-rm -rf $FILE
-
+# Making the folder structure for Installation of goloang and location service
 mkdir -p "$GOPATH/src/github.com/kayako/service-location" "$GOPATH/bin" /geo-db && chmod -R 777 "$GOPATH"
