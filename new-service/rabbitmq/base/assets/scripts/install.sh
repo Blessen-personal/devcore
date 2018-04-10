12b637e51d958ed0d0e0b5cf88b59fec98d11858
kaushik H S <kaushik@kaushiks-MacBook-Pro.local>
2018-04-10 17:00:02 +0530

    adding rabbitmq cluster and standalone for server

---
+++ new-service/rabbitmq/base/assets/scripts/install.sh
@@ -0,0 +1,7 @@
+yum install -y wget unzip tar
+rpm -Uvh https://github.com/rabbitmq/erlang-rpm/releases/download/v19.3.6.5/erlang-19.3.6.5-1.el7.centos.x86_64.rpm
+yum install -y erlang
+rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+yum install -y  https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.0/rabbitmq-server-3.7.0-1.el7.noarch.rpm
+/usr/sbin/rabbitmq-plugins list <<<'y'
+/usr/sbin/rabbitmq-plugins enable --offline rabbitmq_mqtt rabbitmq_stomp rabbitmq_management  rabbitmq_management_agent rabbitmq_federation rabbitmq_federation_management <<<'y'
